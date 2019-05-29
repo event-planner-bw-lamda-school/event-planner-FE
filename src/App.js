1a231a221002b9fc54276ee39677b381e8a7118e
Frankzhac <Frank.hemle@gmail.com>
2019-05-28 20:05:41 -0400

    login and signup for

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import SignUpForm from './components/SignUpForm';
+import Login from './components/Login';
+import corpPlanning from './images/corpPlanning.jpg';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="App__Aside">
+            <img src={corpPlanning} alt="planning" />
+          </div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
+                <NavLink to="/Log-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
+                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+              </div>
+
+              <div className="FormTitle">
+                  <NavLink to="/Log-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Login</NavLink>
+                  or
+                  <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+              </div>
+
+              <Route exact path="/" component={SignUpForm}>
+              </Route>
+              <Route path="/Log-in" component={Login}>
+              </Route>
+          </div>
+
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
